3ed710f1e348d2e9f2524d00a1258d428fc73573
Nnaemeka Asoluka <nasoluka@Nnaemekas-MacBook-Pro.local>
2020-11-03 19:27:49 -0600

    backend added

---
+++ Controllers/image.js
@@ -0,0 +1,31 @@
+const Clarifai = require('clarifai');
+const app = new Clarifai.App({
+    apiKey: '22861ac5775d43c58be4b499e96795e6'
+});
+
+const handleApiCall = (req,res)=>{
+    app.models
+        .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+        .then(data =>{
+            res.json(data)
+        })
+        .catch(err => res.status(400).json('unable to fetch'))
+}
+
+
+const handleImage = (req,res,db)=> {
+    const {id} = req.body;
+    db('users').where('id', '=', id)
+        .increment('entries',1)
+        .returning('entries')
+        .then(entries => {
+            res.json(entries[0]);
+        })
+        .catch(err => res.status(400).json('Unable to get Entries'))
+
+}
+
+module.exports ={
+    handleImage: handleImage,
+    handleApiCall: handleApiCall
+}
---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const cors = require('cors');
+const knex = require('knex');
+const bcrypt = require('bcrypt-nodejs');
+const register = require('./Controllers/register');
+const signin = require('./Controllers/signin')
+const profile = require('./Controllers/profile');
+const image = require('./Controllers/image');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        host:'127.0.0.1',
+        user: 'nasoluka',
+        password: '',
+        database:'smart-brain'
+    }
+});
+
+
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/', (req,res)=>{
+    res.send(db.users);
+})
+
+app.post('/signin', (req,res) => {signin.handleSignin(req,res,db,bcrypt)})
+
+app.post('/register', (req,res) => {register.handleRegister(req,res,db,bcrypt)})
+
+app.get('/profile/:id', (req,res) => {profile.handleProfileGet(req,res,db)})
+
+app.put('/image', (req,res) => {image.handleImage(req,res,db)})
+
+app.post('/imageurl', (req,res) => {image.handleApiCall(req,res)})
+
+app.listen(3000, () => {
+    console.log('app is running on port 3000');
+});
+
+
+/*
+/ --> res = this is working 
+/ --> signin - POST request, respond with SUCCESS or FAIL
+/ --> register --> Post = returns USER (Database which would add user to database )
+/ --> /profile/:userId --> GET = user 
+/ --> image --> PUT (becuase we are updating this keeps count of how many images a user has submitted)
+*/
